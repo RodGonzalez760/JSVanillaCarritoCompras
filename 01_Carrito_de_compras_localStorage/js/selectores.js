2428345c04118022b61c7008f3203f40355433d3
Rodrigo Gonzalez <rgonzalez@kamaleon.cl>
2023-05-04 23:26:21 -0400

    add master

--- 01_Carrito_de_compras_localStorage/js/selectores.js
+++
@@ -1,4 +0,0 @@
-export const carrito = document.querySelector('#carrito');
-export const contenedorCarrito = document.querySelector('#lista-carrito tbody');
-export const btnVaciarCarrito = document.querySelector('#vaciar-carrito');
-export const listaCursos = document.querySelector('#lista-cursos');
---
+++ js/selectores.js
@@ -0,0 +1,4 @@
+export const carrito = document.querySelector('#carrito');
+export const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+export const btnVaciarCarrito = document.querySelector('#vaciar-carrito');
+export const listaCursos = document.querySelector('#lista-cursos');
